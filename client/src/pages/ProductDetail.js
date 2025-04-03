a0f307f9c74076774c93587eca76a60234bc9559
Srikanth Nelluri <nellurisrikanthchowdary007@gmail.com>
2025-04-03 23:47:06 +0200

    Implemented Add to Cart functionality using localStorage

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -1,4 +1,3 @@
-// client/src/pages/ProductDetail.js
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -6,6 +5,7 @@ import axios from 'axios';
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -19,6 +19,18 @@ const ProductDetail = () => {
     fetchProduct();
   }, [id]);
 
+  const addToCart = () => {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const exists = cart.find((item) => item.id === product.id);
+    if (exists) {
+      setMessage('Product already in cart');
+    } else {
+      cart.push({ ...product, quantity: 1 });
+      localStorage.setItem('cart', JSON.stringify(cart));
+      setMessage('Product added to cart');
+    }
+  };
+
   if (!product) return <p>Loading...</p>;
 
   return (
@@ -27,6 +39,8 @@ const ProductDetail = () => {
       <img src={product.image} alt={product.name} style={{ width: '300px', height: '300px' }} />
       <p><strong>Price:</strong> ${product.price}</p>
       <p><strong>Description:</strong> {product.description}</p>
+      <button onClick={addToCart}>Add to Cart</button>
+      <p>{message}</p>
     </div>
   );
 };
